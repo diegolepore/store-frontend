79bf67308fd8d133b66f124749566d6a20134ce2
Diego Palacios <diegopalacios3@gmail.com>
2021-09-17 00:33:11 +0200

    feat: Hide quantity input and add to cart button if no auth user

--- src/app/product-detail-view/product-detail-view.component.ts
+++ src/app/product-detail-view/product-detail-view.component.ts
@@ -1,10 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Product } from '../models/Product';
+import { Auth } from '../models/Auth';
 
 // Services
 import { ProductsService } from '../services/api/products/products.service';
 
+// Store
+import { Store, select } from '@ngrx/store';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-product-detail-view',
   templateUrl: './product-detail-view.component.html',
@@ -13,16 +17,21 @@ import { ProductsService } from '../services/api/products/products.service';
 export class ProductDetailViewComponent implements OnInit {
   id: string
   product!: Product
+  authState$!: Observable<Auth>
+  access_token: string = ''
 
   constructor(
     private route: ActivatedRoute,
     private productsService: ProductsService,
+    private store: Store<{ authState: Auth}>
   ) {
     this.id = ''
   }
 
   ngOnInit(): void {
     this.id = this.route.snapshot.paramMap.get('id') as string
+    this.authState$ = this.store.pipe(select('authState'))
+    this.authState$.subscribe((res) => this.access_token = res.access_token)
 
     this.getProduct(this.id)
   }
