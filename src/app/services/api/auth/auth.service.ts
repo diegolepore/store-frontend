1ff18cb59f99f1da553fecf14d696f472a595d29
Diego Palacios <diegopalacios3@gmail.com>
2021-09-21 06:13:48 +0200

    Delete unused vars

--- src/app/services/api/auth/auth.service.ts
+++ src/app/services/api/auth/auth.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core'
 import { User } from '../../../models/User'
 import { Observable } from 'rxjs'
 import { HttpClient, HttpHeaders } from '@angular/common/http'
-import sign from 'jwt-encode'
 
 const headers = new HttpHeaders({
   'Content-Type': 'application/json'
@@ -10,11 +9,8 @@ const headers = new HttpHeaders({
 
 const options = { headers: headers }
 
-type Login = { email: string, pass: string }
 type Register = { first_name: string, last_name: string, email: string, pass: string }
 type RegisterResponse = { id: number, first_name: string, last_name: string, email: string }
-
-const getJWToken = (obj: User | undefined) => sign({...obj}, 'shhhhh')
 
 @Injectable({
   providedIn: 'root'
